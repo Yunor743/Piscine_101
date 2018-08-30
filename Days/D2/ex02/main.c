981a3dd4d07edd83fff4b48c01b6fa27a8627603
Luffab <tillyfabien.manga@gmail.com>
2018-08-30 17:26:21 +0200

    ex02

---
+++ Days/D2/ex02/main.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void    ft_print_alphabet(void)
+{
+    char alphabet;
+
+    alphabet = '0';
+    while(alphabet <= '9')
+    {
+        ft_putchar(alphabet);
+        alphabet++;
+    }
+}
+int main()
+{
+    ft_print_alphabet();
+}
