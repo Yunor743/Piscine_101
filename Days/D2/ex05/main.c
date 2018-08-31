ff101129c572c98859c28de53c3adb6c280a2f80
Luffab <tillyfabien.manga@gmail.com>
2018-08-31 12:48:32 +0200

    ex05

--- Days/D2/ex05/main.c
+++ Days/D2/ex05/main.c
@@ -1,55 +1,10 @@
 #include <unistd.h>
 
-<<<<<<< HEAD
-void    ft_putchar(char c)
-=======
 void ft_putchar(char c)
->>>>>>> 66d9fbb4433596b3ac6ab5583ad303104d564f12
 {
     write(1, &c, 1);
 }
 
-<<<<<<< HEAD
-void    ft_print_comb2(void)
-{
-    int one;
-    int two;
-    int tree;
-    int four;
-
-    one = 0;
-    two = 0;
-    tree = 0;
-    four = 0;
-    while (one != 9 || two != 8 || tree != 9 || four != 9)
-    {
-        if (tree == 9 && four == 9)
-        {
-            two++;
-            if (two > 9)
-            {
-                one++;
-                two = 0;
-            }
-            tree = 0;
-            four = two;
-        }
-        while(tree != 9 || four != 9)
-        {
-            four++;
-            if (four > 9)
-            {
-                tree++;
-                four = 0;
-            }
-            ft_putchar('0' + one);
-            ft_putchar('0' + two);
-            ft_putchar(' ');
-            ft_putchar('0' + tree);
-            ft_putchar('0' + four);
-
-            if (!((one == 9 && two == 8) && (tree == 9 && four == 9)))
-=======
 void ft_print_comb2(void)
 {
     /*C'est variables représentes les digits de deux nombre sous la forme 00 00*/
@@ -92,7 +47,6 @@ void ft_print_comb2(void)
             ft_putchar(48 + fourth_nrb);
             /*si on la valeur est différente de 98 99 alors on affiche une virgule entre chaque valeur*/
             if(first_nbr != 9 || second_nbr!= 8 || third_nrb != 9 || fourth_nrb != 9)
->>>>>>> 66d9fbb4433596b3ac6ab5583ad303104d564f12
             {
                 ft_putchar(',');
                 ft_putchar(' ');
@@ -104,8 +58,5 @@ void ft_print_comb2(void)
 int main()
 {
     ft_print_comb2();
-<<<<<<< HEAD
-=======
     return 0;
->>>>>>> 66d9fbb4433596b3ac6ab5583ad303104d564f12
 }
