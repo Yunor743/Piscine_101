9ec865b14a49baf9fb9846c89a48a1214f2a8071
Hugo PERINAZZO <hugoperinazzo@gmail.com>
2018-08-31 21:44:18 +0200

    ex07

---
+++ Days/D2/ex07/main.c
@@ -0,0 +1,51 @@
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+/*cette fonction permet de traiter la valeur et renvoi un string des valeur organiser sur n colones,
+ elle renvoi 1 si elle ne peut plus continuer sinon elle renvoie 0*/
+int wlf_process_value(char* storage, int value)
+{
+
+    return 0;
+}
+
+/*on affiche le string*/
+void wlf_disp(char *storage, int digits)
+{
+    int compteur = 0;
+    while(compteur <= digits)
+    {
+        ft_putchar(storage[compteur]);
+        compteur++;
+    }
+}
+
+void ft_print_combn(int n)
+{
+    char storage[n]; //on creer un tableau de n colonnes
+    int value = 0; //on set la valeur de notre nombre
+    int stop; //arret de la boucle
+
+    while(!stop)
+    {
+        value++;
+        stop = wlf_process_value(&storage[0], value);
+        wlf_disp(&storage[0], n);
+        if(!stop)
+        {
+            ft_putchar(',');
+            ft_putchar(' ');
+        }
+    } 
+}
+
+int main(int argc, char *argv[])
+{
+    ft_print_combn(atoi(argv[1]));
+}
