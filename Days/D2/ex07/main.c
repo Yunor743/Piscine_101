ad3946d590e27e6a936c3175a9e7853854d66849
Hugo PERINAZZO <hugoperinazzo@gmail.com>
2018-09-01 13:56:11 +0200

    hugo's work

--- Days/D2/ex07/main.c
+++ Days/D2/ex07/main.c
@@ -1,26 +1,27 @@
-
 #include <unistd.h>
 #include <stdlib.h>
 
+/*This function allow you to write one character in the console*/
 void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
 
-/*cette fonction permet de traiter la valeur et renvoi un string des valeur organiser sur n colones,
- elle renvoi 1 si elle ne peut plus continuer sinon elle renvoie 0*/
+/*This function organize values in 'n' columns of an string,
+it return 1 if it can't continue to execute, else it return 0*/
 int wlf_process(char* storage, int value, int digits)
 {
-    int actual_digit = 0;
-    int val_process[digits];
-    int i = 0; //me servira de compteur
+    int actual_digit = 0;       //the target digit
+    int val_process[digits];    //to organize values
+    int i = 0;                  //i use it for my counter
 
-    /*On initialise tous les digits de notre tableau a 0*/
+    /*We initialize all digits of the array to 0*/
     while(i < digits)
     {
         val_process[i] = 0;
         i++;
     }
+    /*We store the values in different columns*/
     while(value > 0)
     {
         val_process[actual_digit]++;
@@ -36,14 +37,14 @@ int wlf_process(char* storage, int value, int digits)
             actual_digit--;
         }
     }
-    /*On cast val_process vers storage*/
+    /*We cast val_process[] to storage[]*/
     i = 0;
     while(i < digits)
     {
         storage[i] = val_process[i]+48;
         i++;
     }
-    /*on défini la valeur renvoyée*/
+    /*We define the return value*/
     if(val_process[0] < 10)
     {
         return 0;
@@ -54,37 +55,36 @@ int wlf_process(char* storage, int value, int digits)
     }
 }
 
-/*on affiche le string*/
-void wlf_disp(char *storage, int digits)
-{
-    int compteur = digits - 1;
-    while(compteur >= 0)
-    {
-        ft_putchar(storage[compteur]);
-        compteur--;
-    }
-}
-
 void ft_print_combn(int n)
 {
     if(n > 0 && n < 10)
     {
-        char storage[n]; //on creer un tableau de n colonnes
-        int value = 0; //on set la valeur de notre nombre
-        int stop; //arret de la boucle
+        char storage[n];    //my final string to disp
+        int value = 0;      //the value of the number
+        int stop = 0;       //stop the while when we have reach the last combination
+        int counter;
 
         while(!stop)
         {
+            /*for each iteration we increment value and we process it*/
             value++;
             stop = wlf_process(&storage[0], value, n);
+            /*Disp ', ' but note the first and last time*/
             if(!stop && value > 1)
             {
                 ft_putchar(',');
                 ft_putchar(' ');
             }
+            /*Disp 'storage'*/
             if(!stop)
             {
-                wlf_disp(&storage[0], n);
+                /*We start to disp the string by the end*/
+                counter = n - 1;
+                while(counter >= 0)
+                {
+                    ft_putchar(storage[counter]);
+                    counter--;
+                }
             }
         }
     }
