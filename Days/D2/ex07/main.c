8b020a222a1d7162d692c464fe45d65ad02919db
Hugo PERINAZZO <hugoperinazzo@gmail.com>
2018-09-01 13:17:24 +0200

    hugo's work

--- Days/D2/ex07/main.c
+++ Days/D2/ex07/main.c
@@ -9,7 +9,7 @@ void ft_putchar(char c)
 
 /*cette fonction permet de traiter la valeur et renvoi un string des valeur organiser sur n colones,
  elle renvoi 1 si elle ne peut plus continuer sinon elle renvoie 0*/
-int wlf_process_value(char* storage, int value, int digits)
+int wlf_process(char* storage, int value, int digits)
 {
     int actual_digit = 0;
     int val_process[digits];
@@ -21,20 +21,21 @@ int wlf_process_value(char* storage, int value, int digits)
         val_process[i] = 0;
         i++;
     }
-    
     while(value > 0)
     {
         val_process[actual_digit]++;
         value--;
-        if(val_process[actual_digit] > 9)
+        while(val_process[actual_digit] > 9 - actual_digit)
         {
             actual_digit++;
             val_process[actual_digit]++;
-            val_process[actual_digit-1] = val_process[actual_digit]+1;
-            actual_digit = 0;
+        }
+        while(actual_digit != 0)
+        {
+            val_process[actual_digit - 1] = val_process[actual_digit] + 1;
+            actual_digit--;
         }
     }
-    
     /*On cast val_process vers storage*/
     i = 0;
     while(i < digits)
@@ -43,7 +44,7 @@ int wlf_process_value(char* storage, int value, int digits)
         i++;
     }
     /*on défini la valeur renvoyée*/
-    if(val_process[0] < 10)   //condition a modifier
+    if(val_process[0] < 10)
     {
         return 0;
     }
@@ -66,24 +67,27 @@ void wlf_disp(char *storage, int digits)
 
 void ft_print_combn(int n)
 {
-    char storage[n]; //on creer un tableau de n colonnes
-    int value = 0; //on set la valeur de notre nombre
-    int stop; //arret de la boucle
-
-    while(!stop)
+    if(n > 0 && n < 10)
     {
-        value++;
-        stop = wlf_process_value(&storage[0], value, n);
-        if(!stop && value > 1)
+        char storage[n]; //on creer un tableau de n colonnes
+        int value = 0; //on set la valeur de notre nombre
+        int stop; //arret de la boucle
+
+        while(!stop)
         {
-            ft_putchar(',');
-            ft_putchar(' ');
+            value++;
+            stop = wlf_process(&storage[0], value, n);
+            if(!stop && value > 1)
+            {
+                ft_putchar(',');
+                ft_putchar(' ');
+            }
+            if(!stop)
+            {
+                wlf_disp(&storage[0], n);
+            }
         }
-        if(!stop)
-        {
-            wlf_disp(&storage[0], n);
-        }
-    } 
+    }
 }
 
 int main(int argc, char *argv[])
