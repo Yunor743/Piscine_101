a2d927053c8674cc98ee3eb666633eef27dbede6
Hugo PERINAZZO <hugoperinazzo@gmail.com>
2018-09-01 09:30:38 +0200

    hugo's work

--- Days/D2/ex07/main.c
+++ Days/D2/ex07/main.c
@@ -9,7 +9,7 @@ void ft_putchar(char c)
 
 /*cette fonction permet de traiter la valeur et renvoi un string des valeur organiser sur n colones,
  elle renvoi 1 si elle ne peut plus continuer sinon elle renvoie 0*/
-int wlf_process_value(char* storage, int value)
+int wlf_process_value(char* storage, int value, int digits)
 {
 
     return 0;
@@ -35,7 +35,7 @@ void ft_print_combn(int n)
     while(!stop)
     {
         value++;
-        stop = wlf_process_value(&storage[0], value);
+        stop = wlf_process_value(&storage[0], value, n);
         wlf_disp(&storage[0], n);
         if(!stop)
         {
